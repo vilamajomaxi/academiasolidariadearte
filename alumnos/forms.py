b00f9c2bb9785fd7a59dc362c7436c630272ba2c
maxivilamajo <vilamajomaxi@gmail.com>
2016-09-24 15:33:19 -0300

    kaka

--- alumnos/forms.py
+++ alumnos/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from alumnos.models import *
 
-
 class LoginForm(forms.Form):
     username = forms.CharField(label='Usuario', max_length=50, widget=forms.TextInput(
             attrs={'placeholder': 'Ingrese su usuario', 'class': 'form-control'}))
@@ -19,7 +18,33 @@ class AlumnoForm(forms.ModelForm):
         super(AlumnoForm, self).__init__(*args, **kwargs)
         for field in iter(self.fields):
             self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-        })
+                'class': 'form-control'})
 
+
+
+date_choices = (
+            ('0', "enero"),
+            ('1',	"febrero"),
+            ('2',	"marzo"),
+            ('3',	"abril"),
+            ('4',	"mayo"),
+            ('5',	"junio"),
+            ('6',	"julio"),
+            ('7',	"agosto"),
+            ('8',	"septiembre"),
+            ('9',	"octubre"),
+            ('10',"noviembre"),
+            ('11',	"diciembre"),
+)
+
+def Filter_form(forms.Form):
+    mes = forms.ChoiceField(label = "Mes",choice = date_choices,required=False)
+    sede = form.ChoiceField(label = "Sede",choices= sede_choices ,required=False)
+
+
+    def __init__(self, *args, **kwargs):
+        super(AlumnoForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'})
 
--- alumnos/views.py
+++ alumnos/views.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from alumnos.forms import LoginForm, AlumnoForm
+from alumnos.forms import LoginForm, AlumnoForm,Filter_form
 from django.utils.datastructures import MultiValueDictKeyError
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -14,6 +14,7 @@ import datetime
 from alumnos.models import Alumno,Asistencia,Clase
 from django.utils import timezone
 from django.shortcuts import render, redirect
+
 
 
 
@@ -58,8 +59,41 @@ def agregar_alumno(request):
 def reporte(request):
     alumnos = Alumno.objects.all()
     for alumno in alumnos:
-        asistencias = Asistencia.objects.filter(alumno = alumno).()
+        asistencias = Asistencia.objects.filter(alumno = alumno)
         alumno.consolidacion = asistencias.filter(clase__tipo='CV').count()
         alumno.clase =  asistencias.filter(clase__tipo='R').count()
         alumno.devolucion = asistencias.filter(clase__tipo='DS').count()
     return render(request,'dash.html',{'alumnos':alumnos})
+
+
+
+def filter(request):
+    if request.method == 'POST':
+        form = Filter_form(request.POST)
+        if form.is_valid():
+            mes = form.cleaned_data['mes']
+            sede = form.cleaned_data['sede']
+            alumnos = Alumno.objects.all()
+            if sede:
+                alumnos = alumnos.filter(sede = sede)
+            for alumno in alumnos:
+                if mes:
+                    asistencias = Asistencia.objects.filter(alumno=alumno, fecha_hora__month=int(mes) + 1)
+                else:
+                    asistencias = Asistencia.objects.filter(alumno=alumno)
+                asistencias = Asistencia.objects.filter(alumno = alumno)
+                alumno.consolidacion = asistencias.filter(clase__tipo='CV').count()
+                alumno.clase =  asistencias.filter(clase__tipo='R').count()
+                alumno.devolucion = asistencias.filter(clase__tipo='DS').count()
+
+
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('/dash')
+            else:
+                pass
+    else:
+        form=Filter_form()
+    return render(request,'filter.html',{'form':form})
